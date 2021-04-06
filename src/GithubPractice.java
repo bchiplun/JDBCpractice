dbb6c8924a2ad16a8a14a337f9cc583064e41ae0
bchiplun <bchiplunkar7@gmail.com>
2021-04-06 08:47:52 +0530

    original coder commits

--- src/GithubPractice.java
+++ src/GithubPractice.java
@@ -7,6 +7,8 @@ public class GithubPractice {
 		System.out.println("Smal code for github practice by the second user.. changes made after cloning...");
 		
 		System.out.println("Third commmit just for practice");
+		
+		System.out.println("New change from orignal coder");
 
 	}
 
